9e1d620d47253521ba06080d881a57c62b9db4b2
DJ Quardaboff <Spartacrafter@gmail.com>
2018-02-06 17:08:15 -0600

    changed 'PreloadInventoryDatabase'

--- app/src/main/java/com/porterlee/preloadinventory/PreloadInventoryDatabase.java
+++ app/src/main/java/com/porterlee/preloadinventory/PreloadInventoryDatabase.java
@@ -7,8 +7,8 @@ public class PreloadInventoryDatabase {
     public static final String ID = "id";
     public static final String BARCODE = "barcode";
     public static final String LOCATION_ID = "location_id";
-    public static final String PRELOAD_LOCATION_ID = "preload_location_id";
-    public static final String PRELOAD_ITEM_ID = "preload_item_id";
+    public static final String PRELOAD_LOCATION_ID = "preloaded_location_id";
+    public static final String PRELOAD_ITEM_ID = "preloaded_item_id";
     public static final String DESCRIPTION = "description";
     public static final String CASE_NUMBER = "case_number";
     public static final String ITEM_NUMBER = "item_number";
@@ -17,31 +17,29 @@ public class PreloadInventoryDatabase {
 
     public class ScannedItemTable {
         public static final String NAME = "scanned_items";
-        public static final String TABLE_CREATION = NAME + " ( " + ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + LOCATION_ID + " INTEGER, " + PRELOAD_ITEM_ID + " INTEGER, " + BARCODE + " TEXT, " + DATE_TIME + " BIGINT )";
+        public static final String TABLE_CREATION = NAME + " ( " + ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + LOCATION_ID + " INTEGER, " + PRELOAD_ITEM_ID + " INTEGER, " + DATE_TIME + " BIGINT )";
 
         public class Keys {
             public static final String ID = NAME + '.' + PreloadInventoryDatabase.ID;
             public static final String LOCATION_ID = NAME + '.' + PreloadInventoryDatabase.LOCATION_ID;
             public static final String PRELOAD_ITEM_ID = NAME + '.' + PreloadInventoryDatabase.PRELOAD_ITEM_ID;
-            public static final String BARCODE = NAME + '.' + PreloadInventoryDatabase.BARCODE;
             public static final String DATE_TIME = NAME + '.' + PreloadInventoryDatabase.DATE_TIME;
         }
     }
 
     public class ScannedLocationTable {
         public static final String NAME = "scanned_locations";
-        public static final String TABLE_CREATION = NAME + " ( " + ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + PRELOAD_LOCATION_ID + " INTEGER, " + BARCODE + " TEXT, " + DATE_TIME + " BIGINT )";
+        public static final String TABLE_CREATION = NAME + " ( " + ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + PRELOAD_LOCATION_ID + " INTEGER, " + DATE_TIME + " BIGINT )";
 
         public class Keys {
             public static final String ID = NAME + '.' + PreloadInventoryDatabase.ID;
             public static final String PRELOAD_LOCATION_ID = NAME + '.' + PreloadInventoryDatabase.PRELOAD_LOCATION_ID;
-            public static final String BARCODE = NAME + '.' + PreloadInventoryDatabase.BARCODE;
             public static final String DATE_TIME = NAME + '.' + PreloadInventoryDatabase.DATE_TIME;
         }
     }
 
     public class PreloadItemTable {
-        public static final String NAME = "preload_items";
+        public static final String NAME = "preloaded_items";
         public static final String TABLE_CREATION = NAME + " ( " + ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + PRELOAD_LOCATION_ID + " INTEGER, " + BARCODE + " TEXT, " + CASE_NUMBER + " TEXT, " + ITEM_NUMBER + " TEXT, " + PACKAGE + " TEXT, " + DESCRIPTION + " TEXT )";
 
         public class Keys {
@@ -56,7 +54,7 @@ public class PreloadInventoryDatabase {
     }
 
     public class PreloadLocationTable {
-        public static final String NAME = "preload_locations";
+        public static final String NAME = "preloaded_locations";
         public static final String TABLE_CREATION = NAME + " ( " + ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + BARCODE + " TEXT, " + DESCRIPTION + " TEXT )";
 
         public class Keys {
